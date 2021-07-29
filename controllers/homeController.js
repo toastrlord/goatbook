d49cf92c2573b2434df66709d19d7d82b0eb76e9
Rob Morandini <robjmorandini@hotmail.com>
2021-07-29 19:39:37 -0400

    Start setting up home routes

---
+++ controllers/homeController.js
@@ -0,0 +1,35 @@
+const { body, validationResult } = require("express-validator");
+
+exports.home_get = function(req, res, next) {
+    res.render('home');
+}
+
+exports.home_create_account_post = function(req, res, next) {
+    body('username').trim().isLength({min: 10, max: 20}).withMessage('Please enter a username between 10 and 20 characters')
+    .custom((value, {req}) => {
+        User.find({'username': req.body.username})
+        .exec(function (err, name) {
+            if (err) { return next(err); }
+            if (name === body.req.username) {
+                throw new Error('Username is already taken');
+            }
+            return true;
+        });
+    }).withMessage('Username is already taken.').escape(),
+    body('password').trim().isLength({min: 5, max: 20}).withMessage('Please enter a password between 5 and 20 characters long').escape(),
+    body('confirm-password').trim().isLength({min: 5, max: 20}).custom((value, {req}) => {
+        if (value !== req.body.password) {
+            throw new Error('Passwords must match!');
+        }
+        return true;
+    }), (req, res, next) => {
+    }
+}
+
+exports.home_create_account_get = function(req, res, next) {
+    res.render('signup-form');
+}
+
+exports.home_create_post = function(req, res, next) {
+
+}
